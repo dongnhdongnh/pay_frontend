5a74382e2f7c529907968b5a87b36aee264da035
Nguyễn Thị Khuyên Thu <thuntk@vakaxa.com>
2018-09-27 10:47:13 +0000

    Add component verify

--- src/app/vakapay.component/authenticate/verify/verify.component.spec.ts
+++ src/app/vakapay.component/authenticate/verify/verify.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VerifyComponent } from './verify.component';
 
 describe('VerifyComponent', () => {
--- src/app/vakapay.component/authenticate/verify/verify.component.ts
+++ src/app/vakapay.component/authenticate/verify/verify.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './verify.component.html',
   styleUrls: ['./verify.component.css']
 })
+
 export class VerifyComponent implements OnInit {
 
   constructor() { }
