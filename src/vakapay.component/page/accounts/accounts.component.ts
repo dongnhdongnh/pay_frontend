3f77a53f00a9e36edf2125563a99fe81199a9f01
dongnguyen <dongnh@vakaxa.com>
2018-10-16 15:45:57 +0700

    edit account view

--- src/vakapay.component/page/accounts/accounts.component.ts
+++ src/vakapay.component/page/accounts/accounts.component.ts
@@ -15,10 +15,13 @@ import { throwMatDialogContentAlreadyAttachedError } from '@angular/material';
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent extends Root implements OnInit {
+  isDataLoaded: boolean;
   mAccount: Account;
   walletService: WalletService;
   walletsData: any;
+  wallets = new Map<string, string>();
   wallet_current: any;
+  tab_current;
   Coin;
   constructor(
     titleService: Title,
@@ -27,12 +30,12 @@ export class AccountsComponent extends Root implements OnInit {
     mAccountSerive: AccountService,
     walletService: WalletService) {
     super(titleService, route, router);
-   
+
     this.mAccount = mAccountSerive.mAccount;
     this.mAccount.id = "8377a95b-79b4-4dfb-8e1e-b4833443c306";
     this.Coin = "ETH";
     this.walletService = walletService;
-
+    this.isDataLoaded = false;
     // console.log("ACCOUNT ID: " + JSON.stringify(this.mAccount));
     this.getUserData();
   }
@@ -44,26 +47,36 @@ export class AccountsComponent extends Root implements OnInit {
       this.walletsData = JSON.parse(result.message);
       for (var i = 0; i < this.walletsData.length; i++) {
         console.log(this.walletsData[i]);
+        this.wallets.set(this.walletsData[i].Currency, this.nineNumber(this.walletsData[i].Balance));
         // var _w = new Wallet();
         // _w.attributesLower = this.walletsData[i];
         // this.wallets.push(_w);
         // console.log(_w.networkname);
       }
-      await this.getHistory(this.walletsData[0]);
+      this.isDataLoaded = true;
+      this.wallet_current = this.walletsData[0];
+      await this.getHistory(this.wallet_current);
     } catch (error) {
       console.log(error);
     }
-   
+
   }
 
   async getHistory(wallet) {
     try {
-      if (wallet == null)
+      console.log("get history " + JSON.stringify(wallet));
+
+      if (wallet == null) {
+        console.log("nothing to show");
+        this.searchDatas = new Array();
+        this.currentPage = 1;
+        this.totalItems = 0;
         return;
+      }
       this.wallet_current = wallet;
       let walletSearch: any = {};
-      walletSearch.userID =  this.mAccount.id;
-      walletSearch.networkName=this.wallet_current.Currency;
+      walletSearch.userID = this.mAccount.id;
+      walletSearch.networkName = this.wallet_current.Currency;
       walletSearch.offset = (this.currentPage - 1) * this.itemsPerPage;
       walletSearch.limit = this.itemsPerPage;
       walletSearch.orderBy = ['CreatedAt'];
@@ -72,7 +85,8 @@ export class AccountsComponent extends Root implements OnInit {
       this.searchDatas = JSON.parse(result.message);
       this.totalItems = Number(result.data);
       this.searchDatas.forEach(element => {
-      //  element.type = this.wallet_current.Address == element.FromAddress ? 0 : 1;//0:withdran,1:deposit
+        element.type = element.Amount < 0 ? 0 : 1;//0:withdran,1:deposit
+        //element.Amount=Math.abs(element.Amount);
         element.CreatedAt = this.getTime(element.CreatedAt);
       });
     } catch (error) {
@@ -87,8 +101,30 @@ export class AccountsComponent extends Root implements OnInit {
 
   }
 
+  public getBalance(walletName) {
+
+    this.walletsData.forEach(element => {
+      if (element.Currency == walletName) {
+        {
+          console.log("HELLO get Balance " + JSON.stringify(element));
+          //   return JSON.stringify(element);
+          console.log(element.Balance);
+          return element.Balance;
+          //   break;
+        }
+      }
+    })
+    return -1;
+
+  }
+
+  public getJSON(thing) {
+    return JSON.stringify(thing);
+  }
+
   public onClickWalletTab(event, name) {
     console.log(JSON.stringify(event) + name);
+    this.tab_current = name;
     switch (name) {
       case 'VKCW':
         this.updateCurrentWallet("VAKA");
@@ -110,16 +146,19 @@ export class AccountsComponent extends Root implements OnInit {
     }
   }
 
-  updateCurrentWallet(networkName) {
-
+  async updateCurrentWallet(networkName) {
+    this.wallet_current = null;
+    //  if (this.walletsData != null && this.walletsData.length >= 0)
     this.walletsData.forEach(element => {
-      if (element.NetworkName == networkName) {
+      if (element.Currency == networkName) {
         this.wallet_current = element;
-        this.getHistory(this.wallet_current);
+        //    this.getHistory(this.wallet_current);
         console.log(JSON.stringify(this.wallet_current));
         return;
       }
     });
+
+    await this.getHistory(this.wallet_current);
   }
 
   // array of all items to be paged
@@ -149,7 +188,7 @@ export class AccountsComponent extends Root implements OnInit {
 
 
   //#region PAGE
-  searchDatas;
+  searchDatas = new Array();
   currentPage = 1;
   itemsPerPage = 10;
   totalItems = 1;
@@ -157,11 +196,31 @@ export class AccountsComponent extends Root implements OnInit {
 
     this.currentPage = $event;
 
-    await this.getHistory(this.walletsData[0]);
+    await this.getHistory(this.wallet_current);
     // console.log('sthing');
     //  this.searchRewardSetting(this.IdPartnerSearch);
   }
   //#endregion
-
+  nineNumber(x) {
+    if (!x)
+    {
+      return -1;
+    }
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+        x *= Math.pow(10, e - 1);
+        x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10, e);
+        x += (new Array(e + 1)).join('0');
+      }
+    }
+    return x;
+  }
 
 }
